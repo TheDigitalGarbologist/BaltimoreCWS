319661852bf4b86540eb397e86f11ee058601273
Gideon Singer <72046459+TheDigitalGarbologist@users.noreply.github.com>
2024-06-16 19:19:27 -0400

    Update sample.py

--- SpatialDataScience/sample.py
+++ SpatialDataScience/sample.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import pydeck as pdk
+import folium
+from streamlit_folium import folium_static
 import requests
 import pandas as pd
-import colorbrewer
 
 # Function to get GeoJSON data from ArcGIS REST service
 @st.cache_data
@@ -24,6 +24,10 @@ data = pd.json_normalize(geojson_data['features'])
 data['CSA2020'] = data['properties.CSA2020']
 data['wrkout20'] = data['properties.wrkout20']
 
+# Initialize session state for selected tract
+if 'selected_tract' not in st.session_state:
+    st.session_state.selected_tract = 'All'
+
 # Create a Streamlit app
 st.title("Interactive Map of Baltimore City")
 
@@ -31,13 +35,10 @@ st.title("Interactive Map of Baltimore City")
 st.sidebar.title("Filter Community Statistical Area")
 tracts = ['All'] + list(data['CSA2020'].unique())
 selected_tract = st.sidebar.selectbox("Select Community Statistical Area", tracts, index=0)
-
-# Layer toggles
-show_choropleth = st.sidebar.checkbox("Show Choropleth Layer", value=True)
-show_highlight = st.sidebar.checkbox("Show Highlight Layer", value=True)
+st.session_state.selected_tract = selected_tract
 
 # Prepare color ramp (blue chromatic scale)
-color_ramp = colorbrewer.Blues[7]  # Using a 7-class Blues color ramp
+color_ramp = ['#f7fbff', '#deebf7', '#c6dbef', '#9ecae1', '#6baed6', '#3182bd', '#08519c']
 
 # Function to map values to color ramp
 def map_to_color(value, min_val, max_val, color_ramp):
@@ -53,101 +54,56 @@ for feature in geojson_data['features']:
     value = feature['properties']['wrkout20']
     feature['properties']['fill_color'] = map_to_color(value, min_val, max_val, color_ramp)
 
-# Prepare the layers for PyDeck
-layers = []
+# Create a folium map
+m = folium.Map(location=[39.2904, -76.6122], zoom_start=10, tiles='CartoDB positron')
 
-# Add a GeoJsonLayer for the choropleth
-if show_choropleth:
-    choropleth_layer = pdk.Layer(
-        "GeoJsonLayer",
-        geojson_data,
-        pickable=True,
-        stroked=True,
-        filled=True,
-        extruded=False,
-        wireframe=True,
-        get_fill_color="[properties.fill_color[0], properties.fill_color[1], properties.fill_color[2], 200]",  # 33% transparency
-        get_line_color=[0, 0, 0],
-        get_line_width=1,
-    )
-    layers.append(choropleth_layer)
+# Add choropleth layer
+folium.Choropleth(
+    geo_data=geojson_data,
+    name="choropleth",
+    data=data,
+    columns=["CSA2020", "wrkout20"],
+    key_on="feature.properties.CSA2020",
+    fill_color="Blues",
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name="Percent of Employed Residents who Work Outside the City"
+).add_to(m)
 
 # Highlight the selected community statistical area
-if selected_tract != 'All' and show_highlight:
-    selected_geom = next((feature for feature in geojson_data['features'] if feature['properties']['CSA2020'] == selected_tract), None)
+if st.session_state.selected_tract != 'All':
+    selected_geom = next((feature for feature in geojson_data['features'] if feature['properties']['CSA2020'] == st.session_state.selected_tract), None)
     if selected_geom:
-        highlight_layer = pdk.Layer(
-            "GeoJsonLayer",
+        folium.GeoJson(
             selected_geom,
-            pickable=True,
-            stroked=True,
-            filled=True,
-            extruded=False,
-            wireframe=True,
-            get_fill_color=[255, 175, 0, 160],
-            get_line_color=[255, 175, 0],
-            get_line_width=2,
-        )
-        layers.append(highlight_layer)
+            name="highlight",
+            style_function=lambda x: {
+                "fillColor": "#ffaf00",
+                "color": "#ffaf00",
+                "weight": 2,
+                "fillOpacity": 0.6,
+            },
+        ).add_to(m)
 
-# Set the initial view state
-view_state = pdk.ViewState(
-    latitude=39.2904,  # Centered on Baltimore City
-    longitude=-76.6122,
-    zoom=10,
-    pitch=30,
-)
+# Add layer control
+folium.LayerControl().add_to(m)
 
-# Create the deck.gl map with satellite basemap and labels
-deck = pdk.Deck(
-    layers=layers,
-    initial_view_state=view_state,
-    map_style='mapbox://styles/mapbox/satellite-streets-v11',
-    tooltip={"text": "{CSA2020}\n{wrkout20}% of residents work outside the city"},
-)
-
-# Display the map in the Streamlit app
-st.pydeck_chart(deck)
+# Display the map
+folium_static(m)
 
 # Create a map legend
 st.markdown("""
-<style>
-.map-legend {
-    position: fixed;
-    bottom: 20px;
-    left: 20px;
-    z-index: 1000;
-    background: white;
-    padding: 10px;
-    border-radius: 5px;
-    box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
-}
-.map-legend h4 {
-    margin: 0;
-    font-size: 14px;
-    font-weight: bold;
-}
-.map-legend div {
-    display: flex;
-    align-items: center;
-    margin-top: 5px;
-}
-.map-legend span {
-    width: 20px;
-    height: 20px;
-    display: block;
-    margin-right: 5px;
-    border: 1px solid #000;
-}
-</style>
-<div class="map-legend">
+<div style="position: fixed; 
+     bottom: 50px; left: 50px; width: 200px; 
+     background: white; z-index: 1000; padding: 10px; 
+     border: 2px solid grey; border-radius: 5px;">
     <h4>Legend</h4>
-    <div><span style="background-color: #f7fbff"></span>0 - 10%</div>
-    <div><span style="background-color: #deebf7"></span>10 - 20%</div>
-    <div><span style="background-color: #c6dbef"></span>20 - 30%</div>
-    <div><span style="background-color: #9ecae1"></span>30 - 40%</div>
-    <div><span style="background-color: #6baed6"></span>40 - 50%</div>
-    <div><span style="background-color: #3182bd"></span>50 - 60%</div>
-    <div><span style="background-color: #08519c"></span>60 - 70%</div>
+    <i style="background: #f7fbff; width: 18px; height: 18px; float: left; margin-right: 8px; opacity: 0.7;"></i> 0 - 10%<br>
+    <i style="background: #deebf7; width: 18px; height: 18px; float: left; margin-right: 8px; opacity: 0.7;"></i> 10 - 20%<br>
+    <i style="background: #c6dbef; width: 18px; height: 18px; float: left; margin-right: 8px; opacity: 0.7;"></i> 20 - 30%<br>
+    <i style="background: #9ecae1; width: 18px; height: 18px; float: left; margin-right: 8px; opacity: 0.7;"></i> 30 - 40%<br>
+    <i style="background: #6baed6; width: 18px; height: 18px; float: left; margin-right: 8px; opacity: 0.7;"></i> 40 - 50%<br>
+    <i style="background: #3182bd; width: 18px; height: 18px; float: left; margin-right: 8px; opacity: 0.7;"></i> 50 - 60%<br>
+    <i style="background: #08519c; width: 18px; height: 18px; float: left; margin-right: 8px; opacity: 0.7;"></i> 60 - 70%<br>
 </div>
 """, unsafe_allow_html=True)
