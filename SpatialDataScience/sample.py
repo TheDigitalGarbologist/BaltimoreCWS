875f0c3704584eb4c0619dbe60430e8c55c19c68
Gideon Singer <72046459+TheDigitalGarbologist@users.noreply.github.com>
2024-06-16 19:39:49 -0400

    Update sample.py

--- SpatialDataScience/sample.py
+++ SpatialDataScience/sample.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import folium
-from streamlit_folium import folium_static
-import requests
+import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
+import requests
 
 # Function to get GeoJSON data from ArcGIS REST service
 @st.cache_data
@@ -37,136 +37,51 @@ tracts = ['All'] + list(data['CSA2020'].unique())
 selected_tract = st.sidebar.selectbox("Select Community Statistical Area", tracts, index=0)
 st.session_state.selected_tract = selected_tract
 
-# Prepare color ramp (blue chromatic scale)
-color_ramp = ['#f7fbff', '#deebf7', '#c6dbef', '#9ecae1', '#6baed6', '#3182bd', '#08519c']
+# Prepare color ramp
+color_ramp = px.colors.sequential.Blues
 
-# Function to map values to color ramp
-def map_to_color(value, min_val, max_val, color_ramp):
-    color_index = int((value - min_val) / (max_val - min_val) * (len(color_ramp) - 1))
-    return color_ramp[color_index]
-
-# Calculate min and max values for the color mapping
-min_val = data['wrkout20'].min()
-max_val = data['wrkout20'].max()
-
-# Add color to each feature in the geojson data
-for feature in geojson_data['features']:
-    value = feature['properties']['wrkout20']
-    feature['properties']['fill_color'] = map_to_color(value, min_val, max_val, color_ramp)
-
-# Create a folium map with Esri hybrid imagery tiles
-m = folium.Map(location=[39.2904, -76.6122], zoom_start=11, scrollWheelZoom=False)
-
-# Add Esri Imagery tile layer
-folium.TileLayer(
-    tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-    attr='Tiles © Esri',
-    name='Esri Imagery',
-    overlay=False,
-    control=True
-).add_to(m)
-
-# Add choropleth layer
-folium.Choropleth(
-    geo_data=geojson_data,
-    name="choropleth",
-    data=data,
-    columns=["CSA2020", "wrkout20"],
-    key_on="feature.properties.CSA2020",
-    fill_color="Blues",
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name="Percent of Employed Residents who Work Outside the City"
-).add_to(m)
+# Create Plotly map
+fig = px.choropleth_mapbox(
+    data,
+    geojson=geojson_data,
+    locations='properties.CSA2020',
+    featureidkey='properties.CSA2020',
+    color='wrkout20',
+    color_continuous_scale=color_ramp,
+    mapbox_style="satellite-streets",
+    zoom=11,
+    center={"lat": 39.2904, "lon": -76.6122},
+    opacity=0.7,
+    labels={'wrkout20': '% Work Outside City'}
+)
 
 # Highlight the selected community statistical area
 if st.session_state.selected_tract != 'All':
     selected_geom = next((feature for feature in geojson_data['features'] if feature['properties']['CSA2020'] == st.session_state.selected_tract), None)
     if selected_geom:
-        folium.GeoJson(
-            selected_geom,
-            name="highlight",
-            style_function=lambda x: {
-                "fillColor": "#ffaf00",
-                "color": "#ffaf00",
-                "weight": 2,
-                "fillOpacity": 0.6,
-            },
-        ).add_to(m)
+        selected_data = pd.DataFrame([selected_geom['properties']])
+        selected_layer = go.Choroplethmapbox(
+            geojson=geojson_data,
+            locations=selected_data['CSA2020'],
+            z=selected_data['wrkout20'],
+            colorscale=[[0, "orange"], [1, "orange"]],
+            showscale=False,
+            marker_line_width=3,
+            marker_line_color='orange',
+            opacity=0.6,
+            featureidkey='properties.CSA2020'
+        )
+        fig.add_trace(selected_layer)
 
-# Add Esri World Imagery Labels tile layer last to appear above the choropleth
-folium.TileLayer(
-    tiles='https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}',
-    attr='Labels © Esri',
-    name='Esri World Imagery Labels',
-    overlay=True,
-    control=True
-).add_to(m)
+# Update layout for the map
+fig.update_layout(
+    margin={"r":0,"t":0,"l":0,"b":0},
+    coloraxis_colorbar={
+        'title': '% Work Outside City',
+        'tickvals': [0, 10, 20, 30, 40, 50, 60, 70],
+        'ticktext': ['0%', '10%', '20%', '30%', '40%', '50%', '60%', '70%']
+    }
+)
 
-# Add layer control
-folium.LayerControl().add_to(m)
-
-# Custom legend HTML
-legend_html = '''
-<div id="maplegend" class="maplegend" 
-    style="position: fixed; bottom: 50px; left: 50px; z-index:9999; font-size:14px;
-    background-color:rgba(0, 0, 0, 0.5); border:2px solid white; border-radius:6px; padding: 10px; color: white;">
-    <div class="legend-title">Legend</div>
-    <div class="legend-scale">
-      <ul class="legend-labels">
-        <li><span style="background:#f7fbff;"></span>0 - 10%</li>
-        <li><span style="background:#deebf7;"></span>10 - 20%</li>
-        <li><span style="background:#c6dbef;"></span>20 - 30%</li>
-        <li><span style="background:#9ecae1;"></span>30 - 40%</li>
-        <li><span style="background:#6baed6;"></span>40 - 50%</li>
-        <li><span style="background:#3182bd;"></span>50 - 60%</li>
-        <li><span style="background:#08519c;"></span>60 - 70%</li>
-      </ul>
-    </div>
-</div>
-<style type="text/css">
-  .maplegend .legend-title {
-    text-align: left;
-    margin-bottom: 5px;
-    font-weight: bold;
-    font-size: 90%;
-    }
-  .maplegend .legend-scale ul {
-    margin: 0;
-    margin-bottom: 5px;
-    padding: 0;
-    float: left;
-    list-style: none;
-    }
-  .maplegend .legend-scale ul li {
-    font-size: 80%;
-    list-style: none;
-    margin-left: 0;
-    line-height: 18px;
-    margin-bottom: 2px;
-    }
-  .maplegend ul.legend-labels li span {
-    display: block;
-    float: left;
-    height: 16px;
-    width: 30px;
-    margin-right: 5px;
-    margin-left: 0;
-    border: 1px solid #999;
-    }
-  .maplegend .legend-source {
-    font-size: 70%;
-    color: #999;
-    clear: both;
-    }
-  .maplegend a {
-    color: #777;
-    }
-</style>
-'''
-
-# Add custom legend to the map
-m.get_root().html.add_child(folium.Element(legend_html))
-
-# Display the map
-folium_static(m)
+# Display the map in the Streamlit app
+st.plotly_chart(fig, use_container_width=True)
