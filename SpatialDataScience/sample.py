d3f5e8ade95c4614821ffccf6963222948d5f37e
Gideon Singer <72046459+TheDigitalGarbologist@users.noreply.github.com>
2024-06-16 20:10:01 -0400

    Update sample.py

--- SpatialDataScience/sample.py
+++ SpatialDataScience/sample.py
@@ -32,42 +32,26 @@ if 'selected_tract' not in st.session_state:
 # Create a Streamlit app
 st.title("Interactive Map of Baltimore City")
 
-# Sidebar for selecting community statistical area and color ramp
-st.sidebar.title("Map Settings")
+# Sidebar for selecting community statistical area
+st.sidebar.title("Filter Community Statistical Area")
 tracts = ['All'] + list(data['CSA2020'].unique())
 selected_tract = st.sidebar.selectbox("Select Community Statistical Area", tracts, index=0)
 st.session_state.selected_tract = selected_tract
-
-# Color ramp options
-color_ramps = {
-    'Blues': 'Blues',
-    'Viridis': 'Viridis',
-    'Cividis': 'Cividis',
-    'Inferno': 'Inferno',
-    'Magma': 'Magma',
-    'Plasma': 'Plasma'
-}
-selected_color_ramp = st.sidebar.selectbox("Select Color Ramp", list(color_ramps.keys()), index=0)
 
 # Define opacity settings
 fill_opacity = 0.75
 marker_line_opacity = 1.0
 
-# Generate color map for pydeck layer
-color_maps = {
-    'Blues': [[240, 249, 232], [204, 235, 197], [168, 221, 181], [123, 204, 196], [78, 179, 211], [43, 140, 190], [8, 88, 158]],
-    'Viridis': [[253, 231, 37], [94, 201, 98], [33, 145, 140], [59, 82, 139], [68, 1, 84]],
-    'Cividis': [[255, 233, 0], [204, 189, 0], [158, 148, 0], [112, 107, 0], [79, 56, 60]],
-    'Inferno': [[0, 0, 4], [31, 12, 72], [85, 15, 109], [136, 34, 106], [186, 54, 85], [227, 89, 51], [249, 140, 10], [252, 208, 62], [252, 255, 164]],
-    'Magma': [[0, 0, 4], [28, 16, 68], [79, 18, 123], [129, 37, 129], [181, 54, 122], [229, 80, 100], [255, 124, 67], [253, 184, 99], [252, 253, 191]],
-    'Plasma': [[12, 7, 134], [126, 3, 167], [197, 27, 138], [239, 60, 77], [250, 136, 20], [247, 233, 33]]
-}
+# Inferno color ramp
+color_ramp = [[0, 'rgb(0, 0, 4)'], [0.1, 'rgb(31, 12, 72)'], [0.2, 'rgb(85, 15, 109)'], [0.3, 'rgb(136, 34, 106)'], [0.4, 'rgb(186, 54, 85)'], [0.5, 'rgb(227, 89, 51)'], [0.6, 'rgb(249, 140, 10)'], [0.7, 'rgb(252, 208, 62)'], [0.8, 'rgb(252, 255, 164)'], [1, 'rgb(255, 255, 255)']]
 
-def get_color(value, min_val, max_val, color_map):
-    color_idx = int((value - min_val) / (max_val - min_val) * (len(color_map) - 1))
-    return color_map[color_idx] + [int(fill_opacity * 255)]
+def get_color(value, min_val, max_val):
+    color_idx = int((value - min_val) / (max_val - min_val) * (len(color_ramp) - 1))
+    color = color_ramp[color_idx][1]
+    r, g, b = [int(c) for c in color[4:-1].split(',')]
+    return [r, g, b, int(fill_opacity * 255)]
 
-data['color'] = data['wrkout20'].apply(get_color, args=(data['wrkout20'].min(), data['wrkout20'].max(), color_maps[selected_color_ramp]))
+data['color'] = data['wrkout20'].apply(get_color, args=(data['wrkout20'].min(), data['wrkout20'].max()))
 
 # Create pydeck layer
 layer = pdk.Layer(
@@ -105,20 +89,20 @@ with col1:
     # Create the legend labels
     min_val = data['wrkout20'].min()
     max_val = data['wrkout20'].max()
-    steps = len(color_maps[selected_color_ramp])
+    steps = len(color_ramp)
     legend_labels = []
     for i in range(steps):
-        value = min_val + (max_val - min_val) * (i / steps)
-        color = get_color(value, min_val, max_val, color_maps[selected_color_ramp])
+        value = min_val + (max_val - min_val) * (i / (steps - 1))
+        color = get_color(value, min_val, max_val)
         legend_labels.append({'text': f'{value:.1f}', 'color': color})
 
     # Function to create HTML legend
     def create_legend(labels: list) -> str:
-        """Creates an HTML legend from a list dictionary of the format {'text': str, 'color': [r, g, b]}"""
+        """Creates an HTML legend from a list dictionary of the format {'text': str, 'color': [r, g, b, a]}"""
         labels = list(labels)
         for label in labels:
             assert label['color'] and label['text']
-            assert len(label['color']) in (3, 4)
+            assert len(label['color']) == 4
             label['color'] = ', '.join([str(c) for c in label['color']])
         legend_template = jinja2.Template('''
         <style>
